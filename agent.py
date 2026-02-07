61d6ccb223081093c153050c7db000f45e7a45b5
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-02-07 19:09:02 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,15 @@
+from openai import OpenAI
+from prompts import SYSTEM_PROMPT
+import os
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def run_agent(user_message: str):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    return response.choices[0].message.content
