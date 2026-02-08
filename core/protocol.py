e68ac03dad21376269a3678b1a3669cc3726f67e
Vivek kharwar <gamingcodinggymrepeat@gmail.com>
2026-02-09 01:27:05 +0530

    Update protocol.py

--- core/protocol.py
+++ core/protocol.py
@@ -1,6 +1,13 @@
 from typing import Literal, TypedDict
 
-EventType = Literal["token", "error", "done"]
+EventType = Literal[
+    "token",
+    "error",
+    "done",
+    "system",
+    "tool",
+    "plan"
+]
 
 class ChatEvent(TypedDict):
     type: EventType
