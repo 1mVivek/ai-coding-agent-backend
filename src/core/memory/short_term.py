ad44fd508944f486d0e6c5e1ead9238f47c5b76f
Vivek kharwar <gamingcodinggymrepeat@gmail.com>
2026-02-10 01:32:07 +0530

    Implement ShortTermMemory class for message storage

---
+++ src/core/memory/short_term.py
@@ -0,0 +1,11 @@
+class ShortTermMemory:
+    def __init__(self, max_messages: int = 12):
+        self.max_messages = max_messages
+        self.messages = []
+
+    def add(self, role: str, content: str):
+        self.messages.append({"role": role, "content": content})
+        self.messages = self.messages[-self.max_messages :]
+
+    def get(self):
+        return self.messages
